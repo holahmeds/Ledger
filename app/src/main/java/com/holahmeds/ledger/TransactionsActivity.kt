a3cff473e398c852767e4785ed63f6a4d02a4534
Ahmed Rafiq <ahmedrafiq47@gmail.com>
2018-05-08 23:47:20 +0600

    Created empty android project

---
+++ app/src/main/java/com/holahmeds/ledger/TransactionsActivity.kt
@@ -0,0 +1,15 @@
+package com.holahmeds.ledger
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_transactions.*
+
+class TransactionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transactions)
+        setSupportActionBar(toolbar)
+    }
+}
