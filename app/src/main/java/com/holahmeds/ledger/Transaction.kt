fb642d2cbf64e72eccfdfce4de5f440225670156
Ahmed Rafiq <ahmedrafiq47@gmail.com>
2018-06-24 20:53:58 +0600

    Changed what transaction cards look like

--- app/src/main/java/com/holahmeds/ledger/Transaction.kt
+++ app/src/main/java/com/holahmeds/ledger/Transaction.kt
@@ -2,40 +2,33 @@ package com.holahmeds.ledger
 
 import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.*
-import android.content.res.Resources
 import android.os.Parcel
 import android.os.Parcelable
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import com.google.android.flexbox.FlexboxLayout
-import kotlinx.android.synthetic.main.transaction_card.view.*
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-
-fun DPToPixel(size: Int, resources: Resources): Int {
-    return (size * resources.displayMetrics.density).toInt()
-}
 
 @Entity(tableName = "transaction_table")
-class Transaction(
-        @PrimaryKey(autoGenerate = true) var id: Int,
-        val date: LocalDate,
-        val amount: Long,
-        val category: String,
-        val transactee: String?
-        //val tags: List<String> TODO
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readInt(),
-            LocalDate.of(parcel.readInt(), parcel.readInt(), parcel.readInt()),
-            parcel.readLong(),
-            parcel.readString(),
-            parcel.readString()
-    )
+class Transaction: Parcelable {
+    @PrimaryKey(autoGenerate = true) var id: Int
+    val date: LocalDate
+    val amount: Long
+    val category: String
+    val transactee: String?
 
+    constructor(id:Int, date: LocalDate, amount: Long, category: String, transactee: String?) {
+        this.id = id
+        this.date = date
+        this.amount = amount
+        this.category = category
+        this.transactee = transactee
+    }
+
+    constructor(parcel: Parcel) {
+        this.id = parcel.readInt()
+        this.date = LocalDate.of(parcel.readInt(), parcel.readInt(), parcel.readInt())
+        this.amount = parcel.readLong()
+        this.category = parcel.readString()
+        this.transactee = parcel.readString()
+    }
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(id)
         with(date) {
@@ -73,74 +66,4 @@ interface TransactionDao {
 
     @Insert
     fun addAll(transactions: Collection<Transaction>)
-}
-
-class TransactionViewHolder(val transactionView: View): RecyclerView.ViewHolder(transactionView) {
-    val date: TextView = transactionView.date
-    val amount: TextView = transactionView.amount
-    val category: TextView = transactionView.category
-    val transactee: TextView = transactionView.transactee
-    val tags: FlexboxLayout = transactionView.tag_list
-}
-
-class TransactionAdapter(private var data: List<Transaction>, private val onItemLongClick: (Transaction) -> Unit): RecyclerView.Adapter<TransactionViewHolder>() {
-    fun setData(newData: List<Transaction>) {
-        data = newData
-        notifyDataSetChanged()
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
-        val transactionView = LayoutInflater.from(parent.context).inflate(R.layout.transaction_card, parent, false)
-        return TransactionViewHolder(transactionView)
-    }
-
-    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
-        val transaction = data[position]
-
-        holder.date.text = transaction.date.format(DateTimeFormatter.ISO_LOCAL_DATE)
-
-        holder.amount.text = transaction.amount.toString()
-
-        holder.category.text = transaction.category
-
-        holder.transactee.run {
-            if (transaction.transactee != null) {
-                visibility = View.VISIBLE
-                text = transaction.transactee
-            } else {
-                visibility = View.GONE
-            }
-        }
-
-        holder.transactionView.setOnLongClickListener({
-            onItemLongClick(transaction)
-            true
-        })
-
-        /*
-        holder.tags.run {
-            removeAllViews()
-
-            val tagLayoutParams = FlexboxLayout.LayoutParams(FlexboxLayout.LayoutParams.WRAP_CONTENT, FlexboxLayout.LayoutParams.WRAP_CONTENT)
-            val margin = DPToPixel(4, resources)
-            tagLayoutParams.setMargins(margin, margin, margin, margin)
-
-            val padding = DPToPixel(4, resources)
-
-            val borderedBackground = context.resources.getDrawable(R.drawable.rect)
-
-            for (t in transaction.tags) {
-                val newTag = TextView(context).apply {
-                    layoutParams = tagLayoutParams
-                    background = borderedBackground
-                    setPadding(padding, padding, padding, padding)
-                    text = t
-                }
-                holder.tags.addView(newTag)
-            }
-        }
-        */
-    }
-
-    override fun getItemCount() = data.size
 }
---
+++ app/src/main/java/com/holahmeds/ledger/TransactionAdapter.kt
@@ -0,0 +1,54 @@
+package com.holahmeds.ledger
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.transaction_card.view.*
+import java.time.format.DateTimeFormatter
+
+class TransactionViewHolder(val transactionView: View): RecyclerView.ViewHolder(transactionView) {
+    val date: TextView = transactionView.date
+    val amount: TextView = transactionView.amount
+    val category: TextView = transactionView.category
+    val transactee: TextView = transactionView.transactee
+}
+
+class TransactionAdapter(private var data: List<Transaction>, private val onItemLongClick: (Transaction) -> Unit): RecyclerView.Adapter<TransactionViewHolder>() {
+    fun setData(newData: List<Transaction>) {
+        data = newData
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
+        val transactionView = LayoutInflater.from(parent.context).inflate(R.layout.transaction_card, parent, false)
+        return TransactionViewHolder(transactionView)
+    }
+
+    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
+        val transaction = data[position]
+
+        holder.date.text = transaction.date.format(DateTimeFormatter.ISO_LOCAL_DATE)
+
+        holder.amount.text = transaction.amount.toString()
+
+        holder.category.text = transaction.category
+
+        holder.transactee.run {
+            if (transaction.transactee != null) {
+                visibility = View.VISIBLE
+                text = transaction.transactee
+            } else {
+                visibility = View.GONE
+            }
+        }
+
+        holder.transactionView.setOnLongClickListener({
+            onItemLongClick(transaction)
+            true
+        })
+    }
+
+    override fun getItemCount() = data.size
+}
