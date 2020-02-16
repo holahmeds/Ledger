04248c2930ada156c3c1ee93b8718955b46b2a3e
Ahmed Rafiq <ahmedrafiq47@gmail.com>
2020-02-16 23:11:00 +0600

    Update app bar when navigating between fragments

--- app/src/main/java/com/holahmeds/ledger/MainActivity.kt
+++ app/src/main/java/com/holahmeds/ledger/MainActivity.kt
@@ -6,13 +6,20 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
